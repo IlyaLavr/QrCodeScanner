42d08fb839837b4338e51373b39078b92dfe5556
Ilya <iilavr@mail.ru>
2023-03-23 20:54:09 +0400

    added the creation of a qr code from Russian characters

--- QrCodeScanner/Sources/HistoryScreen/HistoryScreenPresenter.swift
+++ QrCodeScanner/Sources/HistoryScreen/HistoryScreenPresenter.swift
@@ -39,7 +39,7 @@ final class HistoryScreenPresenter: HistoryScreenPresenterProtocol {
     }
     
     func fetchQrCodesWithImage() -> [QrCode] {
-        qrCode = model.getAllQrCodes()
+        qrCode = model.getAllQrCodes().reversed()
         return qrCode?.filter { $0.image != nil } ?? []
     }
     
